9c46b5b3af9f64e5e2be09c82eaa313b482240af
Иван Иванов <ivanski34@gmail.com>
2018-07-30 14:29:10 +0300

    Add files via upload

---
+++ NewTableMerge/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace NewTableMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Merge = new System.Windows.Forms.Button();
+            this.button_Browse = new System.Windows.Forms.Button();
+            this.listBox1_excelFile = new System.Windows.Forms.ListBox();
+            this.button_Select = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Merge
+            // 
+            this.button_Merge.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button_Merge.Location = new System.Drawing.Point(372, 102);
+            this.button_Merge.Name = "button_Merge";
+            this.button_Merge.Size = new System.Drawing.Size(75, 23);
+            this.button_Merge.TabIndex = 0;
+            this.button_Merge.Text = "Merge";
+            this.button_Merge.UseVisualStyleBackColor = true;
+            this.button_Merge.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_Browse
+            // 
+            this.button_Browse.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button_Browse.Location = new System.Drawing.Point(372, 17);
+            this.button_Browse.Name = "button_Browse";
+            this.button_Browse.Size = new System.Drawing.Size(75, 23);
+            this.button_Browse.TabIndex = 3;
+            this.button_Browse.Text = "Browse";
+            this.button_Browse.UseVisualStyleBackColor = true;
+            this.button_Browse.Click += new System.EventHandler(this.button_Browse_Click);
+            // 
+            // listBox1_excelFile
+            // 
+            this.listBox1_excelFile.FormattingEnabled = true;
+            this.listBox1_excelFile.Location = new System.Drawing.Point(12, 17);
+            this.listBox1_excelFile.Name = "listBox1_excelFile";
+            this.listBox1_excelFile.Size = new System.Drawing.Size(341, 537);
+            this.listBox1_excelFile.TabIndex = 4;
+            // 
+            // button_Select
+            // 
+            this.button_Select.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.button_Select.Location = new System.Drawing.Point(372, 59);
+            this.button_Select.Name = "button_Select";
+            this.button_Select.Size = new System.Drawing.Size(75, 23);
+            this.button_Select.TabIndex = 5;
+            this.button_Select.Text = "Select all";
+            this.button_Select.UseVisualStyleBackColor = true;
+            this.button_Select.Click += new System.EventHandler(this.button_Select_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(471, 579);
+            this.Controls.Add(this.button_Select);
+            this.Controls.Add(this.listBox1_excelFile);
+            this.Controls.Add(this.button_Browse);
+            this.Controls.Add(this.button_Merge);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Merge;
+        private System.Windows.Forms.Button button_Browse;
+        private System.Windows.Forms.ListBox listBox1_excelFile;
+        private System.Windows.Forms.Button button_Select;
+    }
+}
+
---
+++ NewTableMerge/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NPOI.XSSF.UserModel;
+
+
+
+
+namespace NewTableMerge
+{
+    public partial class Form1 : Form
+    {
+        string[] filename;
+        string[] xlsfiles = new string[] { };
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private static void MergeData(string path, DataTable dt)
+        {
+            // write data in workbook from xls document.
+            XSSFWorkbook workbook = new XSSFWorkbook(path);
+            // read the current table data
+            XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(0);
+            // read the current row data
+            XSSFRow headerRow = (XSSFRow)sheet.GetRow(0);
+            // LastCellNum is the number of cells of current rows
+            int cellCount = headerRow.LastCellNum;
+
+            if (dt.Rows.Count == 0)
+            {
+
+                // build header for there is no data after the first implementation
+                for (int i = headerRow.FirstCellNum; i < cellCount; i++)
+                {
+                    // get data as the column header of DataTable
+                    DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
+
+                    dt.Columns.Add(column);
+                }
+            }
+            else
+            {
+
+                // TODO: check if the subsequent sheet corresponds
+            }
+            // LastRowNum is the number of rows of current table
+            int rowCount = sheet.LastRowNum + 1;
+            for (int i = (sheet.FirstRowNum + 1); i < rowCount; i++)
+            {
+                XSSFRow row = (XSSFRow)sheet.GetRow(i);
+                DataRow dataRow = dt.NewRow();
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                        // get data and convert them into character string type, then save them into the rows of datatable
+                        dataRow[j] = row.GetCell(j).ToString();
+
+
+                }
+                dt.Rows.Add(dataRow);
+            }
+            workbook = null;
+            sheet = null;
+        }
+
+        public static void ExportDataTableToExcel(DataTable dtSource, string strFileName)
+        {
+            // create workbook
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            // the table named mySheet
+            XSSFSheet sheet = (XSSFSheet)workbook.CreateSheet("mySheet");
+            // create the first row
+            XSSFRow dataRow = (XSSFRow)sheet.CreateRow(0);
+            foreach (DataColumn column in dtSource.Columns)
+            {
+                // create the cells in the first row, and add data into these cells circularly
+                dataRow.CreateCell(column.Ordinal).SetCellValue(column.ColumnName);
+
+            }
+            //create rows on the basis of data from datatable(not including table header), and add data into cells in every row
+            for (int i = 0; i < dtSource.Rows.Count; i++)
+            {
+                dataRow = (XSSFRow)sheet.CreateRow(i + 1);
+                for (int j = 0; j < dtSource.Columns.Count; j++)
+                {
+                    dataRow.CreateCell(j).SetCellValue(dtSource.Rows[i][j].ToString());
+                }
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(strFileName, FileMode.Create, FileAccess.Write))
+                {
+
+                    workbook.Write(fs);// write mySheet table in xls document and save it
+                }
+            }
+        }
+
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            // string[] files = new string[] { };
+            for (int i = 0; i < xlsfiles.Length; i++)
+            {
+                MergeData(xlsfiles[i], dt);
+            }
+            ExportDataTableToExcel(dt, @"result.xlsx");
+
+        }
+
+        private void button_Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.xls*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+            string path = Directory.GetCurrentDirectory();
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                xlsfiles = choofdlog.FileNames;
+                filename = choofdlog.SafeFileNames;
+                for (int i = 0; i < xlsfiles.Length; i++)
+                {
+                    listBox1_excelFile.Items.Add(filename[i]);
+                }
+            }
+        }
+
+        private void button_Select_Click(object sender, EventArgs e)
+        {
+            listBox1_excelFile.SelectionMode = SelectionMode.MultiSimple;
+            for (int i = 0; i < listBox1_excelFile.Items.Count; i++)
+                listBox1_excelFile.SetSelected(i, true);
+        }
+    }
+}
